eee9d05e4751d312e985295aa284655c8569cda5
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-07-10 21:47:38 +0300

    solution

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  type    = string
+  default = "mate-terraform-task-2-rg"
+}
+
+variable "location_for_rg" {
+  type    = string
+  default = "West Europe"
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "yaasstorageforterraform"
+}
+
+variable "container_name" {
+  type    = string
+  default = "content"
+}
+
+variable "blob_name" {
+  type    = string
+  default = "my-awesome-content.zip"
+}
