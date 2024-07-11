c22b2b816165e59223a4e8a3a740aa32d07ce4f3
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-07-11 11:58:41 +0300

    added description

--- variables.tf
+++ variables.tf
@@ -1,25 +1,30 @@
 variable "resource_group_name" {
   type    = string
   default = "mate-terraform-task-2-rg"
+  description = "Resource group Name"
 }
 
 variable "location_for_rg" {
   type    = string
   default = "West Europe"
+  description = "Location for the resources"
 }
 
 variable "storage_account_name" {
   type    = string
   default = "yaasstorageforterraform"
+  description = "Storage account Name"
 }
 
 variable "container_name" {
   type    = string
   default = "content"
+  description = "Container Name"
 }
 
 variable "blob_name" {
   type    = string
   default = "my-awesome-content.zip"
+  description = "Blob name"
 }
 
