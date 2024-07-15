a6c3f0098fd9dde4f85d120536d4f47c57aa4ab3
gaupt <olexandrgauptf@gmail.com>
2024-07-15 21:02:32 +0300

    change main and add variable

--- main.tf
+++ main.tf
@@ -7,19 +7,27 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location_for_reg
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "storage_account" {
   name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+resource "azurerm_storage_container" "container" {
+  name = var.container_name
+  storage_account_name = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
+resource "azurerm_storage_blob" "blob" {
+  name = var.blob_name
+  storage_account_name = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.container.name
+  type = "Block"
+  source = "terraform_task_2.zip"
 }
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  type = string
+  default = "meta_terraform_task_2_rg"
+  description = "Resource group Name"
+}
+
+variable "location_for_reg" {
+  type = string
+  default = "West Europe"
+  description = "Location for the resources"
+}
+
+variable "storage_account_name" {
+  type = string
+  default = "gauptstrogeforterraform"
+  description = "Storage account Name"
+}
+
+variable "container_name" {
+  type = string
+  default = "content"
+  description = "Container Name"
+}
+
+variable "blob_name" {
+  type = string
+  default = "my-awesome-content.zip"
+  description = "Blob Name"
+}
