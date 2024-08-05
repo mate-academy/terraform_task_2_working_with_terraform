e6f203f7115e8412f9e209ccc039b3dc3e9436aa
Volkov <volkovyegorandriiovich@gmail.com>
2024-08-05 19:58:32 +0300

    resources created with Terraform apply tfpan

--- main.tf
+++ main.tf
@@ -1,25 +1,35 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.105.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+resource "azurerm_resource_group" "task1" {
+  name     = var.resource_group_name
+  location = var.Region
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "task1" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.task1.name
+  location                 = azurerm_resource_group.task1.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "task1" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.task1.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "task1" {
+  name                   = var.blob_name
+  storage_account_name   = var.storage_account_name
+  storage_container_name = var.container_name
+  type                   = "Block"
+  source                 = "terraform_task_2_working_with_terraform.zip"
 }
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "blob_id" {
+  value = azurerm_storage_blob.task1.id
+}
+
+output "blob_url" {
+  value = azurerm_storage_blob.task1.url
+}
