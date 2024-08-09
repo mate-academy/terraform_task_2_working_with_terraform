7a3b126ed2cdf48d597b24c377689081dff8edb2
cthusq <cthusq.qusevi@gmail.com>
2024-08-09 20:04:38 +0300

    Solution

--- main.tf
+++ main.tf
@@ -1,25 +1,45 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.105.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+resource "azurerm_resource_group" "Tf_Task_St_Container" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "Tf_Task_St_Container" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.Tf_Task_St_Container.name
+  location                 = azurerm_resource_group.Tf_Task_St_Container.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "Tf_Task_St_Container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.Tf_Task_St_Container.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "Tf_Task_St_Container" {
+  name                   = var.blob_name
+  storage_account_name   = azurerm_storage_account.Tf_Task_St_Container.name
+  storage_container_name = azurerm_storage_container.Tf_Task_St_Container.name
+  type                   = "Block"
+  source                 = data.archive_file.Tf_Task_St_Container.output_path
+}
+
+data "archive_file" "Tf_Task_St_Container" {
+  type        = "zip"
+  output_path = "${path.module}/files.zip"
+
+  source {
+    content  = "placeholder"
+    filename = "placeholder.txt"
+  }
 }
